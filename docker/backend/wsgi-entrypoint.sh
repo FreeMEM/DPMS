d5556b1a3aeb402985b0e418ee5222f40a5752ab
freemem <francisco@tapiasbravo.com>
2022-11-05 13:41:55 +0100

    init

---
+++ docker/backend/wsgi-entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# echo `pwd`
+# ls -la
+
+
+until cd /app/DPMS/
+do
+    echo "Waiting for server volume..."
+done
+
+
+if [ -z "${POSTGRES_USER}" ]; then
+    base_postgres_image_default_user='postgres'
+    export POSTGRES_USER="${base_postgres_image_default_user}"
+fi
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+
+
+postgres_ready() {
+python << END
+import sys
+
+import psycopg2
+
+try:
+    psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="${POSTGRES_HOST}",
+        port="${POSTGRES_PORT}",
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+
+END
+}
+until postgres_ready; do
+  >&2 echo 'Waiting for PostgreSQL to become available...'
+  sleep 1
+done
+>&2 echo 'PostgreSQL is available'
+
+exec "$@"
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+./manage.py collectstatic --noinput
+./manage.py runserver 0.0.0.0:8000
+# gunicorn rest-server.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4 --worker-class=gthread
