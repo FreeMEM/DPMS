ef10a3ee1694fd710a7683d9a1bb57d9f43666f8
freemem <francisco@tapiasbravo.com>
2022-11-05 15:50:19 +0100

    settings

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -19,10 +19,10 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 # This allows easy placement of apps within the interior
-# cride directory.
+# dpms directory.
 app_path = os.path.abspath(os.path.join(
     os.path.dirname(os.path.abspath(__file__)), os.pardir))
-sys.path.append(os.path.join(app_path, 'cride'))
+sys.path.append(os.path.join(app_path, 'dpms'))
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
