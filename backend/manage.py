562068c3a450d723de91d5c559e2570ac929b8de
freemem <francisco@tapiasbravo.com>
2022-11-05 15:50:19 +0100

    settings

--- backend/manage.py
+++ backend/manage.py
@@ -26,8 +26,8 @@ if __name__ == "__main__":
         raise
 
     # This allows easy placement of apps within the interior
-    # cride directory.
+    # dpms directory.
     current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, "cride"))
+    sys.path.append(os.path.join(current_path, "dpms"))
 
     execute_from_command_line(sys.argv)
