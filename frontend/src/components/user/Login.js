4c4c67447dacb69969d8e34109cbd63c2e12dfd2
Desde Pegaso <devel@freemem.space>
2024-09-08 01:47:48 +0200

    added google signin button

--- frontend/src/components/user/Login.js
+++ frontend/src/components/user/Login.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../@dpms-freemem/AuthContext";
+import { GoogleIcon } from "./CustomIcons";
 import { Box, Button, TextField, Typography, Paper } from "@mui/material";
+import { useTranslation } from "react-i18next";
+// import Checkbox from "@mui/material/Checkbox";
+import Divider from "@mui/material/Divider";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -22,9 +27,13 @@ const theme = createTheme({
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
-
+  const { t } = useTranslation();
   // Effect to handle shrink behavior if the browser autocompletes the inputs
   useEffect(() => {
     const emailField = document.getElementById("email-field");
@@ -40,6 +49,14 @@ const Login = () => {
     }
   }, []);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -49,6 +66,32 @@ const Login = () => {
       console.error("Login failed:", error);
       // Manejar el error de login aquí
     }
+  };
+  const validateInputs = () => {
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage("Please enter a valid email address.");
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage("");
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage("Password must be at least 6 characters long.");
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage("");
+    }
+
+    return isValid;
   };
 
   return (
@@ -73,7 +116,18 @@ const Login = () => {
           <Typography variant="h5" align="center" gutterBottom>
             Login
           </Typography>
-          <form onSubmit={handleSubmit}>
+
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              gap: 2,
+            }}
+          >
             <TextField
               id="email-field"
               label="Email"
@@ -117,12 +171,37 @@ const Login = () => {
               }}
               autoComplete="current-password"
             />
+            <Box mt={1} mb={2}>
+              <Link to="/forgot-password" style={{ textDecoration: "none", color: "#FFA500" }}>
+                <Typography variant="body2" align="right">
+                  {t("Forgot Password?")}
+                </Typography>
+              </Link>
+            </Box>
             <Box mt={2}>
               <Button type="submit" variant="contained" color="primary" fullWidth>
                 Login
               </Button>
+              <Typography sx={{ textAlign: "center" }}>
+                {t("Don't have an account?")}{" "}
+                <Link to="/forgot-password" style={{ textDecoration: "none", color: "#FFA500" }}>
+                  {t("Sing Up")}
+                </Link>
+              </Typography>
             </Box>
-          </form>
+            <Divider>or</Divider>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                onClick={() => alert("Sign in with Google")}
+                startIcon={<GoogleIcon />}
+              >
+                {t("Sign in with Google")}
+              </Button>
+            </Box>
+          </Box>
         </Paper>
       </Box>
     </ThemeProvider>
