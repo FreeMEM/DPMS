57bd3b279c2587ecde0c407063935a128bc3f629
Desde Pegaso <devel@freemem.space>
2024-09-08 02:14:50 +0200

    more courtains...

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -4,7 +4,21 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+const defaultTheme = createTheme();
 
+const customShadows = [...defaultTheme.shadows];
+export const gray = {
+  50: "hsl(220, 35%, 97%)",
+  100: "hsl(220, 30%, 94%)",
+  200: "hsl(220, 20%, 88%)",
+  300: "hsl(220, 20%, 80%)",
+  400: "hsl(220, 20%, 65%)",
+  500: "hsl(220, 20%, 42%)",
+  600: "hsl(220, 20%, 35%)",
+  700: "hsl(220, 20%, 25%)",
+  800: "hsl(220, 30%, 6%)",
+  900: "hsl(220, 35%, 3%)",
+};
 const theme = createTheme({
   palette: {
     mode: "dark",
@@ -14,9 +28,13 @@ const theme = createTheme({
     secondary: {
       main: "#FFA500", // Naranja vibrante
     },
+    // background: {
+    //   default: "#000000", // Fondo negro
+    //   paper: "#1C1C1C", // Fondo de papel oscuro
+    // },
     background: {
-      default: "#000000", // Fondo negro
-      paper: "#1C1C1C", // Fondo de papel oscuro
+      default: gray[900],
+      paper: "hsl(220, 30%, 7%)",
     },
     text: {
       primary: "#FFFFFF", // Blanco para el texto principal
@@ -24,7 +42,56 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: "Roboto, Arial, sans-serif",
+    fontFamily: ['"Inter", "sans-serif"'].join(","),
+    h1: {
+      fontSize: defaultTheme.typography.pxToRem(48),
+      fontWeight: 600,
+      lineHeight: 1.2,
+      letterSpacing: -0.5,
+    },
+    h2: {
+      fontSize: defaultTheme.typography.pxToRem(36),
+      fontWeight: 600,
+      lineHeight: 1.2,
+    },
+    h3: {
+      fontSize: defaultTheme.typography.pxToRem(30),
+      lineHeight: 1.2,
+    },
+    h4: {
+      fontSize: defaultTheme.typography.pxToRem(24),
+      fontWeight: 600,
+      lineHeight: 1.5,
+    },
+    h5: {
+      fontSize: defaultTheme.typography.pxToRem(20),
+      fontWeight: 600,
+    },
+    h6: {
+      fontSize: defaultTheme.typography.pxToRem(18),
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontSize: defaultTheme.typography.pxToRem(18),
+    },
+    subtitle2: {
+      fontSize: defaultTheme.typography.pxToRem(14),
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: defaultTheme.typography.pxToRem(14),
+    },
+    body2: {
+      fontSize: defaultTheme.typography.pxToRem(14),
+      fontWeight: 400,
+    },
+    caption: {
+      fontSize: defaultTheme.typography.pxToRem(12),
+      fontWeight: 400,
+    },
+  },
+  shape: {
+    borderRadius: 8,
   },
   components: {
     MuiAlert: {
