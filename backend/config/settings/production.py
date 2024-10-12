188e02122e5b070d0c2a99e9fb9981e960a33940
FreeMEM <freemem@gmail.com>
2024-10-12 14:13:16 +0200

    fix: issues on some environment variables

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,19 +1,21 @@
 """Production settings."""
 
 from .base import *  # NOQA
-from .base import env
+import .base import env
 import environ, os
-
-
-env = environ.Env()
-
 from django.core.exceptions import ImproperlyConfigured
+from pprint import pprint
 
+# Base
+# env = environ.Env()
+pprint(env)
 SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+
 if not SECRET_KEY:
     raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
-# Base
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
     default=[
