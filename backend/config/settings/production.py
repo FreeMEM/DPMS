2343fef8b0d4bf9220a16dbc3bcb7fcf80aded5c
FreeMEM <freemem@gmail.com>
2024-10-20 19:17:27 +0200

    feature: settings for sceneid

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -125,7 +125,7 @@ BACKEND_URL = "https://api.dpms.freemem.space"
 # # WhiteNoise
 # MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa F405
 
-INSTALLED_APPS += ["social_django"]
+# INSTALLED_APPS += ["social_django"]
 # Claves de SceneID (obtenidas desde el portal de desarrolladores de SceneID)
 SOCIAL_AUTH_SCENEID_KEY = env("SCENE_CLIENT_ID")
 SOCIAL_AUTH_SCENEID_SECRET = env("SCENEID_CLIENT_SECRET")
