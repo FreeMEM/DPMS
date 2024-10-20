d9c187365a83efa1d707796eebd687d63281b14a
FreeMEM <freemem@gmail.com>
2024-10-20 11:33:10 +0200

    feature: Custom SMTP Connection

--- backend/dpms/backends/email_backend.py
+++ backend/dpms/backends/email_backend.py
@@ -1,18 +1,25 @@
+import smtplib
 import ssl
-
-from django.core.mail.backends.smtp import EmailBackend as SMTPBackend
-from django.utils.functional import cached_property
+from django.core.mail.backends.smtp import EmailBackend
 
 
-class EmailBackend(SMTPBackend):
-    @cached_property
-    def ssl_context(self):
-        if self.ssl_certfile or self.ssl_keyfile:
-            ssl_context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS_CLIENT)
-            ssl_context.load_cert_chain(self.ssl_certfile, self.ssl_keyfile)
-            return ssl_context
-        else:
-            ssl_context = ssl.create_default_context()
-            ssl_context.check_hostname = False
-            ssl_context.verify_mode = ssl.CERT_NONE
-            return ssl_context
+class CustomSMTPConnection(EmailBackend):
+    def open(self):
+        if self.connection:
+            return False
+        try:
+            self.connection = smtplib.SMTP(
+                self.host, self.port, **self.connection_params
+            )
+            if self.use_tls:
+                context = ssl.create_default_context()
+                context.check_hostname = False
+                context.verify_mode = (
+                    ssl.CERT_NONE
+                )  # Deshabilitar verificación del certificado
+                self.connection.starttls(context=context)
+            self.connection.login(self.username, self.password)
+        except:
+            self.close()
+            raise
+        return True
